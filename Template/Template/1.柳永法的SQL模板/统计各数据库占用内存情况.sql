0161bb56bb0ee0e716b2cb9b2ec662279a47745c
yongfa365 <yongfa365@qq.com>
2013-12-23 11:13:51 +0800

    添加 统计数据库占用内存情况

---
+++ Template/Template/1.柳永法的SQL模板/统计各数据库占用内存情况.sql
@@ -0,0 +1,12 @@
+﻿--查询缓冲池内数据库缓冲池中各个数据库的分布情况
+SELECT  DBName = CASE database_id
+                   WHEN 32767 THEN N'ResourchDB'
+                   ELSE DB_NAME(database_id)
+                 END ,
+        COUNT(*) AS cached_pages_count ,
+        COUNT(*) * 8 / 1024 AS [MBUsed] ,
+        SUM(CAST([free_space_in_bytes] AS BIGINT)) / ( 1024 * 1024 ) AS [MBEmpty]
+FROM    sys.dm_os_buffer_descriptors
+GROUP BY DB_NAME(database_id) ,
+        database_id
+ORDER BY cached_pages_count DESC
