de2fac8e241db4a97bfc0bc6961aa747c4df9d88
柳永法 <yongfa365@gmail.com>
2017-11-30 18:37:27 +0800

    Update 整理索引碎片.sql
    添加针对单库重建索引的方法

--- Template/Template/1.柳永法的SQL模板/整理索引碎片.sql
+++ Template/Template/1.柳永法的SQL模板/整理索引碎片.sql
@@ -1,9 +1,6 @@
 ﻿DBCC SHOWCONTIG --查看所有表的索引碎片
 EXEC sp_msforeachtable "DBCC DBREINDEX('?')"  --重建所有表的所有索引
 
-
-
-
 ---------------------------------------------------------
 --不管3721，检查所有，重建所有
 --1.必须断开所有用户的连接.
@@ -29,3 +26,32 @@ Go
 sp_dboption '你的数据库名', 'single user', 'false'
 Go
 
+-----------------------------------------------------------------
+--重建XXXXXXXDB所有表的所有索引
+USE XXXXXXXDB; --Enter the name of the database you want to reindex
+
+DECLARE @TableName VARCHAR(255);
+
+DECLARE TableCursor CURSOR
+FOR
+    SELECT  TABLE_NAME
+    FROM    INFORMATION_SCHEMA.TABLES
+    WHERE   TABLE_TYPE = 'base table';
+
+
+OPEN TableCursor;
+
+FETCH NEXT FROM TableCursor INTO @TableName;
+WHILE @@FETCH_STATUS = 0
+    BEGIN
+        DBCC DBREINDEX(@TableName,' ',90);
+        FETCH NEXT FROM TableCursor INTO @TableName;
+    END;
+
+CLOSE TableCursor;
+DEALLOCATE TableCursor;
+
+--------------------------------------------------------------
+USE XXXXXXXDB;
+DBCC DBREINDEX('TableName',' ',90);
+DBCC DBREINDEX('TableName','indexname',90);
